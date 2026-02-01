24e9d2271e0b846b866b43092e10bfa5f6116cee
Muneeb <muneebshahid5@gmail.com>
2026-02-01 22:34:35 +0100

    Refactor OpenAI response structures: remove unused fields and simplify Content and OpenAIResponse structs

--- src/llms/openai.rs
+++ src/llms/openai.rs
@@ -2,32 +2,12 @@ use serde::Deserialize;
 
 const OPENAI_API_URL: &str = "https://api.openai.com/v1/responses";
 
-//     {
-//         "id": "msg_67b73f697ba4819183a15cc17d011509",
-//         "type": "message",
-//         "role": "assistant",
-//         "content": [
-//             {
-//                 "type": "output_text",
-//                 "text": "Under the soft glow of the moon, Luna the unicorn danced through fields of twinkling stardust, leaving trails of dreams for every child asleep.",
-//                 "annotations": []
-//             }
-//         ]
-//     }
-///
-
 #[derive(Deserialize)]
 pub struct Content {
-    #[serde(rename = "type")]
-    content_type: String,
     pub text: String,
-    annotations: Vec<serde_json::Value>,
 }
 #[derive(Deserialize)]
 pub struct OpenAIResponse {
-    id: String,
-    #[serde(rename = "type")]
-    response_type: String,
     pub content: Option<Vec<Content>>,
 }
 
