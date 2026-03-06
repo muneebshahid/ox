27084ac9727853044388b93eae15218e54e61969
Muneeb <muneebshahid5@gmail.com>
2026-03-06 16:15:10 +0100

    Delete everything

--- src/events/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod agent_bridge;
-pub mod hub;
-pub mod types;
--- src/tui/mod.rs
+++
@@ -1,9 +0,0 @@
-mod input_metrics;
-mod orchestrator;
-mod render;
-mod render_meta;
-mod state;
-mod terminal;
-mod ui_action;
-
-pub use orchestrator::run;
